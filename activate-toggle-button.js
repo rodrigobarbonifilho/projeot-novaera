2b0b92d29656cb63c3b2175929d7e5c33c5e5832
Rodrigo Barboni Filho <68304421+rodrigobarbonifilho@users.noreply.github.com>
2022-01-06 13:23:49 -0300

    Melhoras...
    Remodelei a barra de navegação inteira de novo, criei uma paleta de cores, e deixei a barra de navegação responsiva

--- activate-toggle-button.js
+++ activate-toggle-button.js
@@ -1,9 +1,11 @@
 let show = false;
 
 let toggleButton = document.querySelector(".toggle-button");
+let menuSection = document.querySelector(".menu-section");
 
 toggleButton.addEventListener("click", () => {
     toggleButton.classList.toggle("activate");
+    menuSection.classList.toggle("activate");
 
     show = !show;
 })
