8f9d922c957503db768147d0b0d28a32181f44d5
Rodrigo Barboni Filho <68304421+rodrigobarbonifilho@users.noreply.github.com>
2022-03-11 00:12:22 -0300

    Arrumando as arquivaida, e animando o toggle button

--- activate_toggle_button.js
+++ activate_toggle_button.js
@@ -3,6 +3,8 @@ const links_div = document.querySelector("#collapse-links-js");
 let test = true;
 
 toggle_button.addEventListener("click", () => {
+    toggle_button.classList.toggle("activate", test);
     links_div.classList.toggle("activate", test);
     test = !test;
+
 })
