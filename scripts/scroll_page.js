fe1ab6dc82639235027c297a37ee5be6625497e8
Rodrigo Barboni Filho <68304421+rodrigobarbonifilho@users.noreply.github.com>
2022-03-29 20:31:57 -0300

    Feature do Quem somos added
    Efeito de scroll simples por container
    Pensar futuramente como fazer o efetio de pagina virando

--- scripts/scroll_page.js
+++ scripts/scroll_page.js
@@ -1,12 +1,22 @@
 const indicators = document.querySelectorAll(".indicator");
+const nav = document.getElementById("navbar");
 
-indicators.forEach((indicator) => {
+indicators.forEach((indicator) => { // Adiciona um evento para cada indicador
     indicator.addEventListener("click", () => {
-        let targetContainer = getPos(indicator.firstElementChild.attributes["href"]);
-        window.scroll(0, targetContainer);
+        let targetContainer = indicator.firstElementChild.attributes["href"]; // Pego a posição Y do container
+        let targetContainerY = getPos(targetContainer);
+        let navY = nav.getBoundingClientRect().y;
+
+        if (targetContainer.value === "#info-container") {
+            window.scrollBy(0, navY);
+        } else {
+            window.scrollBy(0, targetContainerY); // Faço a tela dar um scroll até aquela posição
+        }
     })
 })
 
 function getPos(target) {
-    return document.querySelector(`${target.value}`).getBoundingClientRect().y
+    // 0-window.innerHeight - Como passar de um container para o outro
+    let targetY = document.querySelector(`${target.value}`).getBoundingClientRect().y; // pegar a altura do container selecionado em relação a tela do cliente
+    return targetY; // Retorno a altura
 }
