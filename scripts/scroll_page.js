754431a65d4ea82d3bd033941202ddb157f66ff9
Rodrigo Barboni Filho <68304421+rodrigobarbonifilho@users.noreply.github.com>
2022-04-02 17:43:25 -0300

    Features Adicionadas - Quem Somos
    • Texto indicador do conteúdo do próximo container
    • Scroll baseado nos containers do site

--- scripts/scroll_page.js
+++ scripts/scroll_page.js
@@ -1,6 +1,8 @@
 const indicators = document.querySelectorAll(".indicator");
 const nav = document.getElementById("navbar");
+const previewLink = document.querySelectorAll("#preview-content-js");
 
+// Mudando de acordo com os indicadores de cada container
 indicators.forEach((indicator) => { // Adiciona um evento para cada indicador
     indicator.addEventListener("click", () => {
         let targetContainer = indicator.firstElementChild.attributes["href"]; // Pego a posição Y do container
@@ -15,8 +17,24 @@ indicators.forEach((indicator) => { // Adiciona um evento para cada indicador
     })
 })
 
+// Mudando de acordo com os previews container
+previewLink.forEach((link) => {
+    link.addEventListener("click", () => {
+        let targetContainer = link.attributes["target"];
+        console.log(targetContainer.value);
+        let targetContainerY = getPos(targetContainer);
+        let navY = nav.getBoundingClientRect().y;
+
+        if (targetContainer.value === "#info-container") {
+            window.scrollBy(0, navY);
+        } else {
+            window.scrollBy(0, targetContainerY);
+        }
+    })
+})
+
+// Funções
 function getPos(target) {
-    // 0-window.innerHeight - Como passar de um container para o outro
     let targetY = document.querySelector(`${target.value}`).getBoundingClientRect().y; // pegar a altura do container selecionado em relação a tela do cliente
     return targetY; // Retorno a altura
 }
