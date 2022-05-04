5fb9947f1c9d85331ad42afa950dc2ee34d3816b
Rodrigo Barboni Filho <68304421+rodrigobarbonifilho@users.noreply.github.com>
2022-05-03 22:13:08 -0300

    H

---
+++ scripts/expand-button-script.js
@@ -0,0 +1,11 @@
+let buttons = document.querySelectorAll("div.buttons>button");
+let yesButtons = [buttons[0], buttons[2]]
+let noButtons = [buttons[1], buttons[4]]
+let phoneContainer = document.getElementById("phone-contact-container-js");
+let emailContainer = document.getElementById("email-contact-container-js");
+
+yesButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (btn.classList)
+    })
+})
