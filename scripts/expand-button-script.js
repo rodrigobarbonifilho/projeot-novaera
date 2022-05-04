7042e151e70cdffbb0cba9a26488ab9c9a3fd260
Rodrigo Barboni Filho <68304421+rodrigobarbonifilho@users.noreply.github.com>
2022-05-03 23:43:03 -0300

    Modificando para tablets ou telas maiores

--- scripts/expand-button-script.js
+++ scripts/expand-button-script.js
@@ -1,11 +1,30 @@
-let buttons = document.querySelectorAll("div.buttons>button");
-let yesButtons = [buttons[0], buttons[2]]
-let noButtons = [buttons[1], buttons[4]]
-let phoneContainer = document.getElementById("phone-contact-container-js");
-let emailContainer = document.getElementById("email-contact-container-js");
+let phoneContainer = document.getElementById("phone-contact");
+let emailContainer = document.getElementById("email-contact");
+let yesButtonPhone = document.getElementById("yes-button-phone");
+let noButtonPhone = document.getElementById("no-button-phone");
+let yesButtonEmail = document.getElementById("yes-button-email");
+let noButtonEmail = document.getElementById("no-button-email");
+let yesButtons = [yesButtonPhone, yesButtonEmail];
+let noButtons = [noButtonPhone, noButtonEmail];
 
 yesButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
-        if (btn.classList)
+        var btnType = btn.id.split("-");
+        console.log(btnType);
+        if (btnType[2] === "phone") {
+            phoneContainer.classList.toggle("expanded", true);
+            emailContainer.classList.toggle("expanded", false);
+        } else {
+            emailContainer.classList.toggle("expanded", true);
+            phoneContainer.classList.toggle("expanded", false);
+        }
+    })
+})
+
+noButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        document.body.focus();
+        phoneContainer.classList.toggle("expanded", false);
+        emailContainer.classList.toggle("expanded", false);
     })
 })
