18ece130a9a5ac0b2570e80f11b65ce4cdd0c413
Rodrigo Barboni Filho <68304421+rodrigobarbonifilho@users.noreply.github.com>
2022-06-13 23:13:19 -0300

    Começando a estruturar os dados dos cursos mais detalhadamente

--- scripts/showDivs.js
+++ scripts/showDivs.js
@@ -1,7 +1,24 @@
-const knowMoreBtns = document.querySelectorAll(".know-more-btn");
+const linkButtons = document.querySelectorAll(".link-wrapper>a");
+const detailedCourseDivs = document.querySelectorAll(".detailed-courses>div");
+const firstCourse = detailedCourseDivs[0];
+const secondCourse = detailedCourseDivs[1];
+const thirdCourse = detailedCourseDivs[2];
 
-knowMoreBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        
+linkButtons.forEach(linkButton => {
+    linkButton.addEventListener("click", () => {
+        let course = linkButton.getAttribute("href");
+        if (course === "#operacao-de-maquinas-florestais") {
+            firstCourse.classList.toggle("visible", true);
+            secondCourse.classList.toggle("visible", false);
+            thirdCourse.classList.toggle("visible", false);
+        } else if (course === "#manutencao-de-maquinas-florestais") {
+            firstCourse.classList.toggle("visible", false);
+            secondCourse.classList.toggle("visible", true);
+            thirdCourse.classList.toggle("visible", false);
+        } else if (course === "#solda") {
+            firstCourse.classList.toggle("visible", false);
+            secondCourse.classList.toggle("visible", false);
+            thirdCourse.classList.toggle("visible", true);
+        }
     });
-})
+});
